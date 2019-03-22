9de1df23efc7d759fe9996296c873572580b1ab3
Jak Ratiwanich <jak@triangleitc.com>
2019-03-22 14:13:37 -0700

    adding test endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express = require("express");
+//import * as express from 'express';
 
 // export const app = express();
 
 export class App {
-    public  app = express();
+    //public  app = express();
+    public app: express.Application;
 
-    public  test() {
+    constructor() {
+        console.log("Start App constructor");
+        this.app = express();
+        this.testApi();
+        this.testABC();
+        console.log("Finished App constructor");
+    }
+
+    public  testApi() {
         this.app.get( "/", ( req, res ) => {
             // render the index template
             res.send( "index - Hello Typescript express" );
@@ -14,16 +24,29 @@ export class App {
         return this.app;
     }
 
+    public  testABC() {
+        this.app.get( "/abc", ( req, res ) => {
+            // render the index template
+            res.send( {data: "Hello ABC"} );
+        } );
+
+        return this.app;
+    }
+
+    public  testXXX() {
+        this.app.get( "/xxx", ( req, res ) => {
+            // render the index template
+            res.send( {data: "Hello xxxx"} );
+        } );
+
+        return this.app;
+    }
+
 }
 
-const app = new App().test();
+// const app = new App().test();
 
-module.exports = app;
-
-// app.get( "/", ( req, res ) => {
-//     // render the index template
-//     res.send( "index - Hello Typescript express" );
-// } );
+module.exports = new App().app;
 
 /******
  * TEST URL
